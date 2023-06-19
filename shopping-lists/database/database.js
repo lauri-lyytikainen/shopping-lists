c9f05ae3dfb0b13d7ee388bebaeb905c6a27f91c
Lauri Lyytikäinen <lauri.lyytikainen1@gmail.com>
2023-06-19 15:40:08 +0300

    changed env

--- shopping-lists/database/database.js
+++ shopping-lists/database/database.js
@@ -4,9 +4,9 @@ const CONCURRENT_CONNECTIONS = 2;
 
 let connectionPool;
 if (Deno.env.get("DATABASE_URL")) {
-    connectionPool = new Pool({}, CONCURRENT_CONNECTIONS);
-} else {
     connectionPool = new Pool(Deno.env.get("DATABASE_URL"), CONCURRENT_CONNECTIONS);
+} else {
+    connectionPool = new Pool({}, CONCURRENT_CONNECTIONS);
 };
 
 const executeQuery = async (query, params) => {
